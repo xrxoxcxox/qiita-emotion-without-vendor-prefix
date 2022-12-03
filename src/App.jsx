43344b7586245a600315080730c072c28a3ef0f2
Keisuke Watanuki <watanuki@ju-zen.com>
2022-12-03 13:43:34 +0900

    Install Emotion

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import { css } from '@emotion/react'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 css={title}>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -30,5 +31,9 @@ function App() {
     </div>
   )
 }
+
+const title = css`
+  color: #55c500;
+`
 
 export default App
