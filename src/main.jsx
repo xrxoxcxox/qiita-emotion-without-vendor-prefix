dc4b0497bba8b4a5a456b411a4a77f9fc0cf50b2
Keisuke Watanuki <watanuki@ju-zen.com>
2022-12-03 14:11:02 +0900

    Apply createCache

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { CacheProvider } from '@emotion/react'
+import createCache from '@emotion/cache'
+
+export const myCache = createCache({
+  key: 'css',
+  stylisPlugins: [],
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <CacheProvider value={myCache}>
+      <App />
+    </CacheProvider>
+  </React.StrictMode>,
 )
