b7b78f3abcb45b5272ec1930700c1d70257c29d2
Keisuke Watanuki <watanuki@ju-zen.com>
2022-12-03 14:25:53 +0900

    Use TypeScript

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ export const myCache = createCache({
   stylisPlugins: [],
 })
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <CacheProvider value={myCache}>
       <App />
