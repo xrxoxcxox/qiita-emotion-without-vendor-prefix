8cf3e39ab1fd70e551e45cd2a01a06986896b995
Keisuke Watanuki <watanuki@ju-zen.com>
2022-12-03 13:48:28 +0900

    Remove unnecessary HTML and CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import { css } from '@emotion/react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       <div>
@@ -17,23 +14,13 @@ function App() {
         </a>
       </div>
       <h1 css={title}>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
 
 const title = css`
-  color: #55c500;
+  display: flex;
+  justify-content: center;
 `
 
 export default App
